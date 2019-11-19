c692d0b2390beb9fe72de3d585a5e1233e735f4c
adams-coding <asp933@gmail.com>
2019-11-19 13:35:12 +1000

    Added second layer encoding. added numbers to the array of characters for encoding to decrease the chance of the second layer, decoding the first layer by chance.

--- encode.rb
+++ encode.rb
@@ -6,6 +6,11 @@
         user_digits_output = gets.to_i
         return user_digits_output
     end
+    def recipient_screen
+        puts "Enter your recipients last 3 phone digits"
+        recipient_digits_output = gets.to_i
+        return recipient_digits_output
+    end
     
     def user_message
         puts "write your message"
@@ -14,15 +19,29 @@
     
     end
     
-    def encoder(user_text, user_digits)
-            base_letters = [*'a'..'z', *'A'..'Z'].to_a
-            encode_text =" "
+    # Takes the users text and encodes it using their phone digits and the digits of their recipient
+    def encoder(user_text, user_digits, recipient_digits)
+            base_letters = [*'a'..'z', *'0'..'9', *'A'..'Z'].to_a
+            first_text =" "
+            encode_text = " "
+            #first encode with users phone digits
             user_text.each_char do |letter|
+                if letter == " "
+                    first_text += " "
+                else 
+                    base_index = base_letters.find_index(letter)
+                    new_index = ( base_index + user_digits ) % base_letters.count
+                    first_text += base_letters[new_index]
+                end
+                
+            end
+            #second encode with recipients phone digits
+            first_text.each_char do |letter|
                 if letter == " "
                     encode_text += " "
                 else 
                     base_index = base_letters.find_index(letter)
-                    new_index = ( base_index + user_digits ) % base_letters.count
+                    new_index = ( base_index - recipient_digits ) % base_letters.count
                     encode_text += base_letters[new_index]
                 end
                 
