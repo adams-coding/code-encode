da642b93056338ca7c5a0e542f969773a00d6114
adams-coding <asp933@gmail.com>
2019-11-20 23:59:50 +1000

    added tty::prompt menu. created loop to check for saved user data before encoding so as to bypass the need to enter user data

--- main_menu.rb
+++ main_menu.rb
@@ -1,39 +1,79 @@
-def menu_screen 
-    show_menu = true
-        while show_menu == true do
-        menu_options = ["e", "d", "p", "s","q"]
-        puts "---- Main Menu ----"
-        puts "Encode          (e)"
-        puts "Decode          (d)" 
-        puts "Profile         (p)"
-        puts "Saved Messages  (s)"
-        puts "Quit            (q)"
-        puts "Type the first letter of the option you would like to choose and hit enter"
-        menu_choice = gets.chomp
-            case
-            when menu_choice == "e"
-                show_menu = false
-                encode_text = encoder(user_message(),encode_screen(), recipient_screen())
-                puts encode_text
 
-            when menu_choice == "d"
-                decode_text = decoder(decode_screen(), receiver_screen(), sender_message())
-                puts decode_text
+def menu_screen
+    
+    menu_choices = TTY::Prompt.new
+    greeting = "Main Menu",
+    choices = %w(Encode Decode Profile Saved-Messages Quit)
+    answer = menu_choices.select(greeting, choices)
+    if answer == choices[0]
+        user_profile = {}
+        user_profile = profile_return()
+        user_profile_name = user_profile[0]
+        user_profile_digits = user_profile[1]
+        user_profile_digits = user_profile_digits.to_i
+        puts user_profile_digits
 
-            when menu_choice == "p"
-                profile_screen
+        user_profile_exist = user_profile[2]
+        print user_profile
+        if user_profile[2] != nil
+            encode_text = encoder(user_profile_digits, recipient_screen(), user_message())
+            puts encode_text
 
-            when menu_choice == "s"
-                saved_message_screen
-
-            when menu_choice == "q"
-                break
-
-            when menu_options.include?("#{menu_choice}") == false
-                puts "Invalid option. Check brain and fingers then try again"
-                sleep (1)
-                
-            end
+        else
+            encode_text = encoder(encode_screen(), recipient_screen(), user_message())
+            puts encode_text
         end
-        
+        elsif answer == choices[1]
+            decode_text = decoder(decode_screen(), receiver_screen(), sender_message())
+            puts decode_text
+  
+        elsif answer == choices[2]
+            profile_screen
+   
+        elsif answer == choices[3]
+            saved_message_screen
+    
+        elsif answer == choices[4]
+            
+    end
 end
+
+
+
+    # show_menu = false
+        # while show_menu == true do
+        # menu_options = ["e", "d", "p", "s","q"]
+        # puts "---- Main Menu ----"
+        # puts "Encode          (e)"
+        # puts "Decode          (d)" 
+        # puts "Profile         (p)"
+        # puts "Saved Messages  (s)"
+        # puts "Quit            (q)"
+        # puts "Type the first letter of the option you would like to choose and hit enter"
+        # menu_choice = gets.chomp
+        #     case
+        #     when menu_choice == "e"
+        #         show_menu = false
+        #         encode_text = encoder(user_message(),encode_screen(), recipient_screen())
+        #         puts encode_text
+
+        #     when menu_choice == "d"
+        #         decode_text = decoder(decode_screen(), receiver_screen(), sender_message())
+        #         puts decode_text
+
+        #     when menu_choice == "p"
+        #         profile_screen
+
+        #     when menu_choice == "s"
+        #         saved_message_screen
+
+        #     when menu_choice == "q"
+        #         break
+
+        #     when menu_options.include?("#{menu_choice}") == false
+        #         puts "Invalid option. Check brain and fingers then try again"
+        #         sleep (1)
+                
+        #     end
+        # end
+        
