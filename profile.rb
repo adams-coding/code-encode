0c254c72246ded56110edd8458c91053035881f0
adams-coding <asp933@gmail.com>
2019-11-20 23:57:10 +1000

    added method for extracting and editing user profile information. added method for returning userprofile and digits for encoding function

--- profile.rb
+++ profile.rb
@@ -12,7 +12,7 @@ Requirements:
 
 
 =end
-
+require 'csv'
 class Profile
     attr_accessor :name, :digits
     def initialize(name, digits)
@@ -28,5 +28,45 @@ class Friends < Profile
         @digits = digits
     end
 end
-        
-    
+
+
+def profile_screen
+    rows = CSV.open('profile.csv')
+    rows_array = rows.to_a
+
+
+    user_profile = rows_array[1]
+    puts "Hello #{user_profile[0]}! Enter new name"
+    user_change_name_response = gets.chomp
+    puts "Save last 3 phone digits"
+    user_change_digits_response = gets.chomp
+
+    desired_indices = [1] 
+
+    edited_rows = rows_array.each_with_index.map do |row, index| 
+    if desired_indices.include?(index)
+
+        # Return variables
+        row[0] = user_change_name_response
+        row[1] = user_change_digits_response
+        row[2] = true
+
+        # store index in each edited rows to keep track of the rows
+        [index, row]
+
+    end
+    end.compact
+
+    # update the main row_array with updated data
+    edited_rows.each{|row| rows_array[row[0]] = row[1]}
+
+    # now update the file
+    CSV.open('profile.csv', 'wb') { |csv| rows_array.each{|row| csv << row}}
+end
+
+def profile_return
+    rows = CSV.open('profile.csv')
+    rows_array = rows.to_a
+    user_profile = rows_array[1]
+    return user_profile
+end
